f6b2d674e799811aa6aaaea16ed204fdf750aff0
Marissa Nolan <ritz1484@cs.com>
2021-01-20 01:54:07 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,9 +31,11 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  assembled_aoh[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  
 end
