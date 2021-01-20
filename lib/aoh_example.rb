8fa183d68d87406b8a3cd63c21c60f8b491a4f87
Marissa Nolan <ritz1484@cs.com>
2021-01-20 01:59:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -29,6 +29,7 @@ def literal_aoh
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
+    p lit_AoH
 end
 
 def aoh_lookup(aoh, row, key)
